c0447d51e742e3e814042afc85f6b2af829ebb5c
JuanCaballeroN <juansebastiancaballeronieto@gmail.com>
2023-05-22 17:50:56 -0500

    primericimo commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// vanilla JS
+// init with element
+var elem = document.querySelector('.grid-conteiner');
+var msnry = new Masonry( elem, {
+  // options...
+  itemSelector: '.grid-item82',
+  columnWidth: 230,
+  gutter: 20,
+  isFitWidth: true
+});
+
